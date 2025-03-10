86ac076de6e45cde83923209599fb2e0cc9610bc
Lawton Nichols <lawtonnichols@gmail.com>
2025-03-10 12:11:09 -0700

    .

---
+++ csci1/lec14/square.py
@@ -0,0 +1,55 @@
+import turtle
+
+print(turtle.position())
+
+# make the turtle actually look like a turtle
+turtle.shape("turtle")
+
+# make the turtle go fast
+turtle.speed('fast')
+
+# Mr. Turtle, please move in your current direction by 100 units
+turtle.forward(100)
+
+print(turtle.position())
+
+# turn left 90 degrees
+turtle.left(90)
+
+turtle.forward(100)
+
+turtle.left(90)
+
+turtle.forward(100)
+
+turtle.left(90)
+
+turtle.forward(100)
+
+# let's draw a second square!
+
+turtle.setheading(0)
+
+turtle.up() # pick your tail up
+turtle.forward(150)
+turtle.down() # put your tail down
+
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(25)
+
+
+
+
+
+
+
+
+
+
+
+
